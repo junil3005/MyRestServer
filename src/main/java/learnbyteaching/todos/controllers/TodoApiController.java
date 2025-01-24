9d8de22a4895c8d1a258a305e51f3e7169df12ad
junil3005 <yjl3005@naver.com>
2025-01-24 16:39:28 +0900

    CORS Policy 처리

--- src/main/java/learnbyteaching/todos/controllers/TodoApiController.java
+++ src/main/java/learnbyteaching/todos/controllers/TodoApiController.java
@@ -2,16 +2,29 @@ package learnbyteaching.todos.controllers;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import learnbyteaching.todos.repositories.dao.TodoRepository;
 import learnbyteaching.todos.repositories.vo.TodoItem;
+//	CORS 설정 : 원칙적으로는 허용할 도메인을 정확하게 명시해야 한다.
+@CrossOrigin(origins="*", //	모든 호스트로부터의 오리진 허가)
+	methods= {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE },
+)
 
 @RequestMapping("/api/todos")
 @RestController	//	@Controller + @ResponseBody
@@ -36,5 +49,50 @@ public class TodoApiController {
 		Optional<TodoItem> todo = todoRepository.findById(id);
 		return todo.map(ResponseEntity::ok)
 				.orElseGet(() -> ResponseEntity.notFound().build());
+	}
+	
+	//	keyword 파라미터 전달 받아서 title을 대상으로 검색 -> 목록
+	@GetMapping("/search")
+	public ResponseEntity<List<TodoItem>> searchTodos(@RequestParam("keyword") String keyword) {
+		List<TodoItem> foundTodos = todoRepository.findAll().stream().filter(todo -> todo.getTitle().toLowerCase().contains(keyword.toLowerCase()))
+		.collect(Collectors.toList());
+		return ResponseEntity.ok(foundTodos);
+		
+	}
+	
+	//	새로운 TodoItem 생성
+	@PostMapping
+	public ResponseEntity<TodoItem> createTodo(@RequestBody TodoItem todoItem) {
+		TodoItem savedTodo = todoRepository.save(todoItem);
+		
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.header("Location", "/api/todos/" + savedTodo.getId())
+				.body(savedTodo);
+		//	TODO: 나중에 수정
+	}
+	
+	//	기존 TodoItem 수정
+	@PutMapping("/{id}")	//	api/todos/{id}
+	public ResponseEntity<TodoItem> updateTodo(
+			@PathVariable Long id, @RequestBody TodoItem updatedTodo) {
+		return todoRepository.findById(id)
+				.map(todo -> {
+					todo.setTitle(updatedTodo.getTitle());
+					todo.setCompleted(updatedTodo.isCompleted());
+					TodoItem savedTodo = todoRepository.save(todo);
+					return ResponseEntity.ok(savedTodo);
+				})
+				.orElseGet(() -> ResponseEntity.notFound().build());
+	}
+	
+	//	기존 TodoItem 항목 삭제
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable Long id) {
+		Optional<TodoItem> existingTodo = todoRepository.findById(id);
+		if (!existingTodo.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		todoRepository.deleteById(id);
+		return ResponseEntity.ok().<Void>build();
 	}
 }
